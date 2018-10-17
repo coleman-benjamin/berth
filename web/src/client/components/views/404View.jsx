1c39c1d1e557c0dbf0bc115ba31628f98ce6dab1
Benjamin Coleman <colemanjben@gmail.com>
2018-10-16 23:25:22 -0700

    move 404 500

---
+++ web/src/client/components/views/404View.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class View404 extends Component {
+    render() {
+        return (
+            <div id="fourOhFour">
+                <h1>Cannot be found</h1>
+            </div>
+        )
+    }
+}
+
+export default View404;
---
+++ web/src/client/components/views/500View.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class View500 extends Component {
+    render() {
+        return (
+            <div id="fiveHundad">
+                <h1>Broke it</h1>
+                <p>Something really broke, but not all is broken.</p>
+            </div>
+        )
+    }
+}
+
+export default View500;
