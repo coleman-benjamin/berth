7f706db2c6a3d707704dc74026bd3c827b315cdd
Benjamin Coleman <colemanjben@gmail.com>
2019-03-19 22:07:52 -0700

    change settings

--- games/src/settings/StartScene.js
+++ games/src/settings/StartScene.js
@@ -1,9 +1,20 @@
 import Phaser from 'phaser';
+import * as dat from 'dat.gui';
 
 class StartScene extends Phaser.Scene {
 
     constructor() {
         super();
+        this.settings = null;
+        this.numSettings = null;
+        this.rows = null;
+        this.store = {};
+        this.maxSettings = 100;
+        this.maxRows = 10;
+        this.animFrames = null;
+
+        this.counter = 0;
+        this.mod = 60;
     }
 
     preload() {
@@ -38,34 +49,62 @@ class StartScene extends Phaser.Scene {
     drawPoints(points) {
         const group = this.add.group();
         for (let point of points) {
-            let setting = this.add.sprite(point.x, point.y, 'sprites').play("anim");
+            let setting = this.add.sprite(point.x, point.y, 'sprites');
+            let startFrame = Phaser.Math.RND.between(0, this.animFrames.length);
+            setting.play("anim", false, startFrame);
             group.add(setting);
         }
         return group;
     }
 
     create() {
-        const numSettings = 3; // can only be integer
-        const rows = 1; // can only divide numSettings to integer (what's the term?)
+        // TODO: cache
+        this.numSettings = 2; // can only be integer
+        this.rows = 1; // can only divide numSettings to integer (what's the term?)
+        this.updateStore();
 
+        this.animFrames = this.anims.generateFrameNames('sprites');
         this.anims.create({
             key : "anim",
-            frames: this.anims.generateFrameNames('sprites'),
+            frames: this.animFrames,
             frameRate: 3,
             repeat: -1
         });
 
-        let points = this.calculatePoints(numSettings, rows);
-        let settings = this.drawPoints(points);
+        let points = this.calculatePoints(this.numSettings, this.rows);
+        this.settings = this.drawPoints(points);
 
-        // TODO: Actually make this work with DOM
-        // window.addEventListener("message", (e) => {
-        //     if (e.data.update) {
-        //         points = this.calculatePoints(e.data.update.numSettings, e.data.update.rows);
-        //         settings.destroy();
-        //         settings = this.drawPoints(points);
-        //     }
-        // });
+        const gui = new dat.GUI();
+        gui.add(this, "numSettings", this.numSettings, this.maxSettings).step(1);
+        gui.add(this, "rows", this.rows, this.maxRows).step(1);
+    }
+
+    updateStore() {
+        this.store = {
+            numSettings: this.numSettings,
+            rows: this.rows,
+        };
+    }
+
+    rotate() {
+        for (let setting of this.settings.getChildren()) {
+            setting.angle = Phaser.Math.RND.between(-180, 180);
+        }
+    }
+
+    update() {
+        if (this.counter % this.mod === 0) {
+            if (this.store.numSettings !== this.numSettings || this.store.rows !== this.rows) { // check settings changed
+                if (this.numSettings % this.rows === 0) { // check valid settings
+                    this.updateStore();
+                    let points = this.calculatePoints(this.numSettings, this.rows);
+                    this.settings.destroy(true);
+                    this.settings = this.drawPoints(points);
+                    this.rotate();
+                }
+            }
+        }
+        this.counter ++;
     }
 }
 
