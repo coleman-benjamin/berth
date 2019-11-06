f103f6467372fcbd5c5d7660548a86127c8a395d
Benjamin Coleman <colemanjben@gmail.com>
2019-11-06 09:36:32 -0800

    cleanup generator module

--- games/generator/index.js
+++ games/generator/index.js
@@ -1,25 +1,25 @@
-const prompt = require("prompt");
 const fs = require("fs-extra");
-const moment = require("moment");
 const path = require("path");
+const prompt = require("prompt");
+const moment = require("moment");
 
 const MODULE_PATH = path.resolve(__dirname, "../module") + "/";
 const TEMPLATE_PATH = path.resolve(__dirname, "template.js");
 const TEMPLATE_SEPARATOR = ";;;\n";
 const FILENAMES = {
-    MAIN: "main.js",
-    META: "meta.json",
-    START_SCENE: "start_scene.js"
+	MAIN: "main.js",
+	META: "meta.json",
+	START_SCENE: "start_scene.js"
 };
 
 const inputSchema = {
-    properties: {
-        name: {
-            pattern: /^[a-zA-Z\s\-]+$/,
-            message: 'Game name must be only letters, spaces, or dashes',
-            required: true
-        }
-    }
+	properties: {
+		name: {
+			pattern: /^[a-zA-Z\s\-]+$/,
+			message: 'Game name must be only letters, spaces, or dashes',
+			required: true
+		}
+	}
 };
 
 prompt.start();
@@ -27,48 +27,39 @@ prompt.start();
 prompt.get(inputSchema, createGameBase);
 
 async function createGameBase(err, input) {
-    if (err) {
-        console.log(err);
-        return;
-    }
+	if (err) {
+		console.log(err);
+		return;
+	}
 
-    console.log("Creating base for " + input.name);
+	console.log("Creating base for " + input.name);
 
-    // Configure name for game directory
-    const game_id = input.name.toLowerCase().replace(/ /, "_");
-    const game_dir = MODULE_PATH + game_id + "/";
+	// Configure name for game directory
+	const game_id = input.name.toLowerCase().replace(/ /, "_");
+	const game_dir = path.normalize(MODULE_PATH + game_id + "/");
 
-    // Content for meta file, prettify for write
-    const meta = JSON.stringify({
-        "id": game_id,
-        "title": input.name,
-        "created_at": moment().format()
-    }, null, 2);
+	// Content for meta file, prettify for write
+	const meta = JSON.stringify({
+		"id": game_id,
+		"title": input.name,
+		"created_at": moment().format()
+	}, null, 2);
 
-    try {
-        // Create game directory
-        await fs.mkdir(game_dir);
+	try {
+		// Create game directory
+		await fs.mkdir(game_dir);
 
-        // Get template file, 
-        const file = await fs.readFile(TEMPLATE_PATH);
-        const body = new Buffer(file).toString("utf8");
-        const template = body.split(TEMPLATE_SEPARATOR)
+		// Get template file, 
+		const body = await fs.readFile(TEMPLATE_PATH, "utf8");
+		const template = body.split(TEMPLATE_SEPARATOR)
 
-        // File write operations
-        const writes = [
-            fs.writeFile(game_dir + FILENAMES.MAIN, template[0]),
-            fs.writeFile(game_dir + FILENAMES.START_SCENE, template[1]),
-            fs.writeFile(game_dir + FILENAMES.META, meta),
-        ];
+		// File write operations
+		await fs.writeFile(game_dir + FILENAMES.MAIN, template[0]);
+		await fs.writeFile(game_dir + FILENAMES.START_SCENE, template[1]);
+		await fs.writeFile(game_dir + FILENAMES.META, meta);
 
-        Promise.all(writes)
-            .then(() => {
-                console.log(`Wrote template files to ${game_dir}`)
-            })
-            .catch((e) => {
-                console.log(e);
-            })
-    } catch (e) {
-        console.log(e);
-    }
+		console.log(`Wrote template files to ${game_dir}`)
+	} catch (e) {
+		console.log(e);
+	}
 }
