dde2d5c0e714790d80f80300ef0cc789e21693c3
coleman-benjamin <colemanjben@gmail.com>
2019-12-04 11:23:26 -0800

    update deprecated component update fn

--- client/src/views/GameView.jsx
+++ client/src/views/GameView.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 
 class GameView extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    shouldComponentUpdate() {
-        return false;
-    }
+	shouldComponentUpdate() {
+		return false;
+	}
 
-    componentWillReceiveProps(nextProps) {
-        if (this.props !== nextProps) {
-            this.props = nextProps;
+	componentDidUpdate(nextProps) {
+		if (this.props !== nextProps) {
+			this.props = nextProps;
 
-            //Size iframe to fill the content frame
-            let contentFrame = document.querySelector("#frame");
-            this.iFrame.width = contentFrame.clientWidth;
-            this.iFrame.height = contentFrame.clientWidth / 2;
+			//Size iframe to fill the content frame
+			let contentFrame = document.querySelector("#frame");
+			this.iFrame.width = contentFrame.clientWidth;
+			this.iFrame.height = contentFrame.clientWidth / 2;
 
-            // Load game data into iFrame
-            this.iFrame.onload = () => {
-                this.iFrame.contentWindow.postMessage(this.props.game, "*");
-            };
-        }
-    }
+			// Load game data into iFrame
+			this.iFrame.onload = () => {
+				this.iFrame.contentWindow.postMessage(this.props.game, "*");
+			};
+		}
+	}
 
-    render() {
-        return (
-            <iframe
-                ref={(f) => this.iFrame = f }
-                src='/game.html'
-                scrolling='no'
-                frameBorder='0'
-            >
-            </iframe>
-        )
-    }
+	render() {
+		return (
+			<iframe
+				ref={(f) => this.iFrame = f}
+				src='/game.html'
+				scrolling='no'
+				frameBorder='0'
+			>
+			</iframe>
+		)
+	}
 }
 
 export default GameView;
