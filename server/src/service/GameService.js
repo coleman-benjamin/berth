1170c9e89ea09cb04271cd8ebf1e342f0f753629
coleman-benjamin <colemanjben@gmail.com>
2019-12-04 11:49:28 -0800

    fix exception naming, remove unneeded class members

--- server/src/service/GameService.js
+++ server/src/service/GameService.js
@@ -1,16 +1,14 @@
 const path = require("path");
 const FileSystem = require("../database/FileSystem");
-const { ResourceNotFoundException } = require("../exception");
+const { ResourceNotFound } = require("../exception");
+
+const fsAdapter = new FileSystem(path.resolve(__dirname, "../../data"));
+const COLLECTION_NAME = "games";
 
 class GameService {
-	constructor() {
-		this.adapter = new FileSystem(path.resolve(__dirname, "../../data"));
-		this.gamesCollectionName = "games";
-	}
-
 	async getAll() {
 		try {
-			const records = await this.adapter.fetchCollection(this.gamesCollectionName);
+			const records = await fsAdapter.fetchCollection(COLLECTION_NAME);
 			return Promise.resolve(Object.keys(records).map(r => records[r]));
 		} catch (e) {
 			return Promise.reject(e);
@@ -19,8 +17,8 @@ class GameService {
 
 	async getById(id) {
 		try {
-			const records = await this.adapter.fetchCollection(this.gamesCollectionName);
-			if (!records[id]) throw new ResourceNotFoundException();
+			const records = await fsAdapter.fetchCollection(COLLECTION_NAME);
+			if (!records[id]) throw new ResourceNotFound();
 			return Promise.resolve(records[id]);
 		} catch (e) {
 			return Promise.reject(e);
